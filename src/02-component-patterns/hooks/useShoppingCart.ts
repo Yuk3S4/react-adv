ff996d973898ac30dff0ed7750599302d9c9daa0
Marco Daniel <marco11.mendia@gmail.com>
2022-10-09 20:28:43 -0500

    Control Props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -7,31 +7,17 @@ export const useShoppingCart = () => {
 
     const onProductCountChange = ({ count, product }: { count:number, product: Product }) => {
         
-        setShopingCart( oldShopingCart => {
-
-            const productInCart: ProductInCart = oldShopingCart[ product.id ] || { ...product, count: 0 };
-
-            if( Math.max( productInCart.count + count, 0 ) > 0 ) {
-                productInCart.count += count;
-                return {
-                    ...oldShopingCart,
-                    [product.id]: productInCart
-                }
+        setShopingCart( oldShopingCart => {   
+            
+            if( count === 0 ) {
+                const { [product.id]: toDelete, ...rest } = oldShopingCart;
+                return rest;
             }
 
-            // Borrar el producto   
-            const { [product.id]: toDelete, ...rest } = oldShopingCart;
-            return rest;         
-            
-            // if( count === 0 ) {
-            //     const { [product.id]: toDelete, ...rest } = oldShopingCart;
-            //     return rest;
-            // }
-
-            // return {
-            //     ...oldShopingCart,
-            //     [ product.id ]: { ...product, count }
-            // }
+            return {
+                ...oldShopingCart,
+                [ product.id ]: { ...product, count }
+            }
         })
 
     }
